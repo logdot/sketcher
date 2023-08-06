c7a4b725564fc58c6efea8e98de769a2481f9c99
Timoteo Rider Sweeney <timoteo2.25@gmail.com>
2023-08-06 01:42:29 -0600

    Implement basic size slider

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-var length = 10;
+let length = 10;
+let lengthInput = document.getElementById('length-input');
+lengthInput.value = length;
 
 let container = document.querySelector('.container');
 
-container.style.cssText = `grid-template-columns: repeat(${length}, 1fr); grid-template-rows: repeat(${length}, 1fr);`;
+function GenerateGrid() {
+    container.style.cssText = `grid-template-columns: repeat(${length}, 1fr); grid-template-rows: repeat(${length}, 1fr);`;
 
-for (let i = 0; i < length * length; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'red';
-    });
+    for (let i = 0; i < length * length; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'red';
+        });
 
-    container.appendChild(cell);
+        container.appendChild(cell);
+    }
+}
+
+function ClearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+GenerateGrid();
+
+lengthInput.onchange = () => {
+    length = lengthInput.value;
+
+    ClearGrid();
+    GenerateGrid();
 }
