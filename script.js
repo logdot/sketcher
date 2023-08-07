717a35ede1e17c8a7099d86e3a018d9f7a638eda
Timoteo Rider Sweeney <timoteo2.25@gmail.com>
2023-08-06 23:19:36 -0600

    Add shade generator

--- script.js
+++ script.js
@@ -18,14 +18,29 @@ function RainbowGenerator(currColor) {
     return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
 }
 
+function ShadeGenerator(currColor) {
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(currColor);
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
+
+    red = Math.max(red - 25, 0);
+    green = Math.max(green - 25, 0);
+    blue = Math.max(blue - 25, 0);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
 function GenerateGrid() {
     container.style.cssText = `grid-template-columns: repeat(${length}, 1fr); grid-template-rows: repeat(${length}, 1fr);`;
 
     for (let i = 0; i < length * length; i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.style.backgroundColor = "rgb(255, 255, 255)";
+
         cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = RainbowGenerator();
+            cell.style.backgroundColor = ShadeGenerator(cell.style.backgroundColor);
         });
 
         container.appendChild(cell);
